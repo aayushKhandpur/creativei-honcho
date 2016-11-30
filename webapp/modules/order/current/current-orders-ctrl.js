1a1273745a53937b7419cf6d93314d9892939002
keshavgr8 <keshavgr8@gmail.com>
2016-12-01 00:35:39 +0530

    Changed Order State to Current Order State Tables are now generated using ng-Repeat Fixed Login Button colour bug on login screen Changed background colour for occupied tables

--- webapp/modules/order/current/current-orders-ctrl.js
+++ webapp/modules/order/current/current-orders-ctrl.js
@@ -1,4 +1,52 @@
 'use strict';
 creativei_app.controller('CurrentOrdersController', function($scope){
   console.log("Inside current orders controller.");
+    
+    
+    //my changes
+    //dummy json for tables
+    $scope.tables = [
+    {
+        "tableNo": 1,
+        "tableOccupancy": 4,
+        "isAvailable": false,
+        "tableStatus": "Table",
+        "tableOccupiedDuration": "1:30Hrs"
+    },
+    {
+        "tableNo": 2,
+        "tableOccupancy": 4,
+        "isAvailable": false,
+        "tableStatus": "Table",
+        "tableOccupiedDuration": "1:30Hrs"
+    },
+    {
+        "tableNo": 3,
+        "tableOccupancy": 4,
+        "isAvailable": false,
+        "tableStatus": "Table",
+        "tableOccupiedDuration": "1:30Hrs"
+    },
+    {
+        "tableNo": 4,
+        "tableOccupancy": 4,
+        "isAvailable": true,
+        "tableStatus": "null",
+        "tableOccupiedDuration": "null"
+    },
+        {
+        "tableNo": 5,
+        "tableOccupancy": 4,
+        "isAvailable": false,
+        "tableStatus": "Table",
+        "tableOccupiedDuration": "1:30Hrs"
+    },
+    {
+        "tableNo": 6,
+        "tableOccupancy": 4,
+        "isAvailable": true,
+        "tableStatus": "null",
+        "tableOccupiedDuration": "null"
+    }
+]
 });
