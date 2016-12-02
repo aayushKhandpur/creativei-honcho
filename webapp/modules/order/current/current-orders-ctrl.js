621f6ce60c1225efe9f9167f32de7fee7215977e
keshavgr8 <keshavgr8@gmail.com>
2016-12-02 11:37:06 +0530

    Implemented Table Status Filter Fine tuned the filter css.

--- webapp/modules/order/current/current-orders-ctrl.js
+++ webapp/modules/order/current/current-orders-ctrl.js
@@ -4,6 +4,27 @@ creativei_app.controller('CurrentOrdersController', function($scope){
     
     
     //my changes
+    //filter implementation
+    $scope.tableFilterOptions = ['All', 'Available', 'Occupied'];
+    $scope.tableFilter = $scope.tableFilterOptions[0];
+    $scope.tableFilterFunction = function (element) {
+        if ($scope.tableFilter == 'All') {
+            return true;
+        } else if ($scope.tableFilter == 'Occupied') {
+            if (element.isAvailable) {
+                return false;
+            } else {
+                return true;
+            }
+        } else if ($scope.tableFilter == 'Available') {
+            if (element.isAvailable) {
+                return true;
+            } else {
+                return false;
+            }
+}
+    };
+    //end filter implementation
     //dummy json for tables
     $scope.tables = [
     {
@@ -16,9 +37,9 @@ creativei_app.controller('CurrentOrdersController', function($scope){
     {
         "tableNo": 2,
         "tableOccupancy": 4,
-        "isAvailable": false,
-        "tableStatus": "Table",
-        "tableOccupiedDuration": "1:30Hrs"
+        "isAvailable": true,
+        "tableStatus": null,
+        "tableOccupiedDuration": null
     },
     {
         "tableNo": 3,
@@ -31,22 +52,22 @@ creativei_app.controller('CurrentOrdersController', function($scope){
         "tableNo": 4,
         "tableOccupancy": 4,
         "isAvailable": true,
-        "tableStatus": "null",
-        "tableOccupiedDuration": "null"
+        "tableStatus": null,
+        "tableOccupiedDuration": null
     },
         {
         "tableNo": 5,
         "tableOccupancy": 4,
-        "isAvailable": false,
-        "tableStatus": "Table",
-        "tableOccupiedDuration": "1:30Hrs"
+        "isAvailable": true,
+        "tableStatus": null,
+        "tableOccupiedDuration": null
     },
     {
         "tableNo": 6,
         "tableOccupancy": 4,
         "isAvailable": true,
-        "tableStatus": "null",
-        "tableOccupiedDuration": "null"
+        "tableStatus": null,
+        "tableOccupiedDuration": null
     }
 ]
 });
