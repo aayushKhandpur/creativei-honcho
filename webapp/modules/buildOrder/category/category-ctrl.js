4bc1feb2adf47c98181ff1528f57a096ad1e0305
keshavgr8 <keshavgr8@gmail.com>
2016-12-15 23:57:27 +0530

    Rebuilt category.view.html using bootstrap carousel and ng-repeat

--- webapp/modules/buildOrder/category/category-ctrl.js
+++ webapp/modules/buildOrder/category/category-ctrl.js
@@ -1,3 +1,9 @@
-creativei_app.controller('CategoryController', function($scope){
+creativei_app.controller('CategoryController', function($scope,$http){
   console.log("Inside category controller.");
+    
+    //dummy json for categories
+    $http.get("../../../commons/JSONs/categories.json")
+    .then(function(response){
+        $scope.categories = response.data;
+    });
 });
