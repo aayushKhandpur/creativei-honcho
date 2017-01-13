b9d3f32f498cbbadebd7edf0c9f9c16ecd4a801a
aayush <aayush.khandpur@gmail.com>
2017-01-13 13:21:58 +0530

    Updated serachService method

--- webapp/modules/services/services-ctrl.js
+++ webapp/modules/services/services-ctrl.js
@@ -3,7 +3,16 @@ creativei_app.controller("ServicesController", function ($scope,$state) {
     //  console.log(servObj);
     $scope.services = ["order"];
     //dummy comment
-    $scope.selectOrderService = function(){
-      $state.go('order.current');  
+    $scope.selectService = function(service){
+
+      switch (service) {
+        case 'ORDER':
+          $state.go('order.current');
+          break;
+        default:
+          console.log("no state selected.");
+
+      }
+
     };
 });
