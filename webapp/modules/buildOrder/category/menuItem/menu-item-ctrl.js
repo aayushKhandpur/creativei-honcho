ad8e480b8bdba58e46762f45dfa16d97fd8b34de
keshavgr8 <keshavgr8@gmail.com>
2017-02-16 14:46:00 +0530

    menu item screen with dynamic categories and menu items

--- webapp/modules/buildOrder/category/menuItem/menu-item-ctrl.js
+++ webapp/modules/buildOrder/category/menuItem/menu-item-ctrl.js
@@ -1,9 +1,16 @@
 creativei_app.controller('MenuItemController', function($scope,$stateParams,$http){
     console.log("Inside menu item controller.");
     console.log($stateParams);
-    $http.get('../../../../commons/JSONs/menuItem.json')
+    $http.get('../../../../commons/JSONs/menuCategory.json')
+    .then(function(response){
+        $scope.categories = response.data; 
+        console.log(response.data);
+    });
+    $http.get('../../../../commons/JSONs/menuMenuItem.json')
     .then(function(response){
         $scope.menuItems = response.data; 
         console.log(response.data);
     });
+    
+    $scope.isCollapsed = true;
 });
