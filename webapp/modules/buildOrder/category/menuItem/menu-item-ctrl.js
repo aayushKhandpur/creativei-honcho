c26fa94192e0ae5491c9d61c6bab1c3f112b801f
keshavgr8 <keshavgr8@gmail.com>
2017-02-16 15:16:03 +0530

    Customisation modal added to menu item screen

--- webapp/modules/buildOrder/category/menuItem/menu-item-ctrl.js
+++ webapp/modules/buildOrder/category/menuItem/menu-item-ctrl.js
@@ -1,16 +1,44 @@
-creativei_app.controller('MenuItemController', function($scope,$stateParams,$http){
+creativei_app.controller('MenuItemController', function ($scope, $uibModal,$stateParams,$http) {
     console.log("Inside menu item controller.");
+
     console.log($stateParams);
+    
+    //fetch category and menu item data from the JSONs
     $http.get('../../../../commons/JSONs/menuCategory.json')
-    .then(function(response){
+        .then(function(response){
         $scope.categories = response.data; 
         console.log(response.data);
     });
     $http.get('../../../../commons/JSONs/menuMenuItem.json')
-    .then(function(response){
+        .then(function(response){
         $scope.menuItems = response.data; 
         console.log(response.data);
     });
     
+    //condition for the category aside collapse
     $scope.isCollapsed = true;
+
+    //selection and customisation modal js
+    $scope.customiseDish = function (menuItemId) {
+        var modalInstance = $uibModal.open({
+            //to set this true, you will need to add ngAnimate module
+            animation: false,
+            backdrop: 'static',                                         //disables dismissing modal by clicking on backdrop
+            templateUrl: 'modules/buildOrder/selectionAndCustomisation/customisationModal.view.html',
+            controller: 'CustomisationModalController',
+            size: 'md'
+        });
+        
+        console.log(menuItemId);
+
+        modalInstance.result.then(function (status) {
+            if(status){
+                console.log('You clicked on add to cart');              //log when the modal is dismissed with add to cart button
+            }else{
+                console.log('You clicked on close button');             //log when the modal is dismissed with close button
+            }
+        }, function () {
+            console.log('Modal dismissed at: ' + new Date());           //log when the modal is dismissed by clicking on the modal backdrop,set backdrop as true or false to enable
+        });
+    };
 });
---
+++ webapp/modules/buildOrder/selectionAndCustomisation/customisation-modal-ctrl.js
@@ -0,0 +1,14 @@
+creativei_app.controller('CustomisationModalController', function($scope,$uibModalInstance){
+    console.log("Inside customisation modal controller.");
+    $scope.itemQuantity = 1;
+    console.log("Modal Opened at "+ new Date());
+    $scope.ok = function () {                       //function called on add to cart button
+      //send the status as true
+      $uibModalInstance.close( true );
+    };
+
+    $scope.cancel = function () {                   //function called on closing the modal via close button
+      //send the status as false
+      $uibModalInstance.close( false );
+    }; 
+});
