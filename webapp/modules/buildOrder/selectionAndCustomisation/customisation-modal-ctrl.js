f97c8c96a5ec102fb15742d141658c6a794080d3
keshavgr8 <keshavgr8@gmail.com>
2017-02-21 22:30:28 +0530

    Added the function to increase and decrease quantity

--- webapp/modules/buildOrder/selectionAndCustomisation/customisation-modal-ctrl.js
+++ webapp/modules/buildOrder/selectionAndCustomisation/customisation-modal-ctrl.js
@@ -1,7 +1,23 @@
-creativei_app.controller('CustomisationModalController', function($scope,$uibModalInstance,CartService){
+creativei_app.controller('CustomisationModalController', function($scope,$uibModalInstance,CartService,menuItemId){
     console.log("Inside customisation modal controller.");
+   
+    //quantity control
     $scope.itemQuantity = 1;
+    $scope.increaseQuantity = function(){
+        return $scope.itemQuantity++;
+    };
+    $scope.decreaseQuantity = function(){
+        if($scope.itemQuantity == 1){
+            return;
+        }else{
+            return $scope.itemQuantity--;
+        }
+    };
+    //quantity control
+    
+    $scope.menuItemId = menuItemId;                 //gets menu item id from the menu-item-ctrl
     console.log("Modal Opened at "+ new Date());
+    
     $scope.ok = function () {                       //function called on add to cart button
       CartService.addItem();
       //send the status as true
