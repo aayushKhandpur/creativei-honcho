3f858945221353eaed2fec087f7ab48a81037d56
aayush <aayush.khandpur@gmail.com>
2017-02-22 00:33:48 +0530

    linking menuitem with modal

--- webapp/modules/buildOrder/category/menuItem/menu-item-ctrl.js
+++ webapp/modules/buildOrder/category/menuItem/menu-item-ctrl.js
@@ -2,33 +2,43 @@ creativei_app.controller('MenuItemController', function ($scope, $uibModal,$stat
     console.log("Inside menu item controller.");
 
     console.log($stateParams);
-    
+
     //fetch category and menu item data from the JSONs
     $http.get('../../../../commons/JSONs/menuCategory.json')
         .then(function(response){
-        $scope.categories = response.data; 
+        $scope.categories = response.data;
         console.log(response.data);
     });
     $http.get('../../../../commons/JSONs/menuMenuItem.json')
         .then(function(response){
-        $scope.menuItems = response.data; 
+        $scope.menuItems = response.data;
         console.log(response.data);
     });
-    
+
     //condition for the category aside collapse
     $scope.isCollapsed = true;
 
     //selection and customisation modal js
     $scope.customiseDish = function (menuItemId) {
+
+      if(menuItemId == undefined || menuItemId === "")
+        return "Error";
+
+        $scope.selectedMenuItemId = menuItemId;
         var modalInstance = $uibModal.open({
             //to set this true, you will need to add ngAnimate module
             animation: false,
             backdrop: 'static',                                         //disables dismissing modal by clicking on backdrop
             templateUrl: 'modules/buildOrder/selectionAndCustomisation/customisationModal.view.html',
             controller: 'CustomisationModalController',
-            size: 'md'
+            size: 'md',
+            resolve: {
+              menuItemId:function(){
+              var menuItemId =$scope.selectedMenuItemId;
+              return menuItemId;
+            }}
         });
-        
+
         console.log(menuItemId);
 
         modalInstance.result.then(function (status) {
