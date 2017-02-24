f54af8cc54bd18836d1b61979bf68057342f6d28
aayush <aayush.khandpur@gmail.com>
2017-02-24 23:48:42 +0530

    modal close handling dummy

--- webapp/modules/buildOrder/category/menuItem/menu-item-ctrl.js
+++ webapp/modules/buildOrder/category/menuItem/menu-item-ctrl.js
@@ -1,4 +1,4 @@
-creativei_app.controller('MenuItemController', function ($scope, $uibModal,$stateParams,$http) {
+creativei_app.controller('MenuItemController', function ($scope, $uibModal,$stateParams,$http,CartService) {
     console.log("Inside menu item controller.");
 
     console.log($stateParams);
@@ -39,14 +39,14 @@ creativei_app.controller('MenuItemController', function ($scope, $uibModal,$stat
             }}
         });
 
-        console.log(menuItemId);
-
-        modalInstance.result.then(function (status) {
-            if(status){
-                console.log('You clicked on add to cart');              //log when the modal is dismissed with add to cart button
-            }else{
-                console.log('You clicked on close button');             //log when the modal is dismissed with close button
-            }
+        modalInstance.result.then(function (data) {
+            console.log(data);
+            CartService.addItem();
+            // if(status){
+            //     console.log('You clicked on add to cart');              //log when the modal is dismissed with add to cart button
+            // }else{
+            //     console.log('You clicked on close button');             //log when the modal is dismissed with close button
+            // }
         }, function () {
             console.log('Modal dismissed at: ' + new Date());           //log when the modal is dismissed by clicking on the modal backdrop,set backdrop as true or false to enable
         });
--- webapp/modules/buildOrder/selectionAndCustomisation/customisation-modal-ctrl.js
+++ webapp/modules/buildOrder/selectionAndCustomisation/customisation-modal-ctrl.js
@@ -1,6 +1,6 @@
 creativei_app.controller('CustomisationModalController', function($scope,$uibModalInstance,CartService,menuItemId){
     console.log("Inside customisation modal controller.");
-   
+
     //quantity control
     $scope.itemQuantity = 1;
     $scope.increaseQuantity = function(){
@@ -14,18 +14,23 @@ creativei_app.controller('CustomisationModalController', function($scope,$uibMod
         }
     };
     //quantity control
-    
+
     $scope.menuItemId = menuItemId;                 //gets menu item id from the menu-item-ctrl
     console.log("Modal Opened at "+ new Date());
-    
-    $scope.ok = function () {                       //function called on add to cart button
-      CartService.addItem();
-      //send the status as true
-      $uibModalInstance.close( true );
+
+    $scope.ok = function () {
+      //function called on add to cart button
+      $uibModalInstance.close({
+                menuItemId : $scope.menuItemId,
+                quantity : 1,
+                custom: {spice:"mild", additional: "Dummy information"}
+            });
+
+
     };
 
     $scope.cancel = function () {                   //function called on closing the modal via close button
       //send the status as false
-      $uibModalInstance.close( false );
-    }; 
+      $uibModalInstance.dismiss();
+    };
 });
