5555b5f4b55877bb4354a8109b9d7aee3759e996
aayush <aayush.khandpur@gmail.com>
2017-03-09 01:15:42 +0530

    Menu item and json updates Bootstrap added Add cart method updated. states updated to point directly to menu.

--- webapp/modules/cart/cart-service.js
+++ webapp/modules/cart/cart-service.js
@@ -1,9 +1,28 @@
-creativei_app.factory('CartService', function($http){
+creativei_app.factory('CartService', function($http, $rootScope){
   var cart = {};
 
-  cart.addItem = function (){
+  cart.addItem = function (menuItem, tableId){
+
     console.log("Item added to cart.");
-    return {message: "Item added"};
+    if($rootScope.runningOrders === undefined )
+      $rootScope.runningOrders = {};
+    if(angular.isUndefined($rootScope.runningOrders[tebleId]))
+      $rootScope.runningOrders[tebleId] = {};
+    if(angular.isUndefined($rootScope.runningOrders[tebleId][menuItem.id])){
+      $rootScope.runningOrders[tebleId][menuItem.id] = {
+                                                          name: menuItem.name,
+                                                          quantity: menuItem.quantity,
+                                                          isVeg: menuItem.isVeg,
+                                                          rate: menuItem.price,
+                                                          price: menuItem.price*menuItem.quantity
+                                                        };
+    }else{
+      $rootScope.runningOrders[tebleId][menuItem.id]["quantity"]= menuItem.quantity;
+      $rootScope.runningOrders[tebleId][menuItem.id]["rate"]= menuItem.price;
+      $rootScope.runningOrders[tebleId][menuItem.id]["price"]=menuItem.price*menuItem.quantity;
+    }
+
+    return {message: "Item added", item:$rootScope.runningOrders[tebleId] };
   };
 
   cart.removeItem = function (){
