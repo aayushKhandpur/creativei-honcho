b18d3b7f670ebd676e50d17da8f4f68836b31cfe
keshavgr8 <keshavgr8@gmail.com>
2017-03-30 14:11:50 +0530

    Fixed rating system

--- webapp/modules/buildOrder/feedback/feedback-ctrl.js
+++ webapp/modules/buildOrder/feedback/feedback-ctrl.js
@@ -1,3 +1,15 @@
-creativei_app.controller('feedbackController', function ($scope) {
+creativei_app.controller('feedbackController', function ($scope, $http) {
     console.log("Inside feedback controller.");
+    //dummy json for feedback criteria
+    $http.get("../../../commons/JSONs/feedback.json")
+    .then(function(response) {
+        $scope.feedbackQuestions = response.data;
+    });
+    $scope.criteria1 = 0;
+    $scope.criteria2 = 0;
+    $scope.criteria3 = 0;
+    $scope.criteria4 = 0;
+    $scope.additonalComment = "";
+    $scope.titles = ['Bad', 'Meh', 'Okay', 'Good', 'Excellent']
+    $scope.maxRating = 5;
 });
