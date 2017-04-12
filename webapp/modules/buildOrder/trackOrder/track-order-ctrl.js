70da785a5a694ff207c7abb9b23a13b7a7987a88
keshavgr8 <keshavgr8@gmail.com>
2017-04-13 02:21:23 +0530

    recommended item

--- webapp/modules/buildOrder/trackOrder/track-order-ctrl.js
+++ webapp/modules/buildOrder/trackOrder/track-order-ctrl.js
@@ -1,6 +1,24 @@
-creativei_app.controller('OrderTrackerController', function($scope, $rootScope, CartService){
-  console.log("Inside order tracker controller.");
-  $scope.tableId = "1";
-  $scope.cartItems = $rootScope.runningOrders[$scope.tableId].items;
-  $scope.subtotal = CartService.updateSubTotal($scope.cartItems);
+creativei_app.controller('OrderTrackerController', function ($scope, $rootScope, CartService) {
+    console.log("Inside order tracker controller.");
+    $scope.tableId = "1";
+    $scope.cartItems = $rootScope.runningOrders[$scope.tableId].items;
+    $scope.subtotal = CartService.updateSubTotal($scope.cartItems);
+    
+    
+    
+    //recommended item
+    $scope.recommendedItems = [
+        'Coke',
+        'Fries',
+        'Iced Tea',
+        'Strawberry Smoothie',
+        'Thumsup',
+        'Grilled Sandwich',
+        'Chicken Sandwich'
+    ];
+    
+    $scope.addRecommendedItem = function(index){
+        $scope.addedItem = $scope.recommendedItems.splice(index,1);
+        console.log($scope.addedItem);
+    }
 });
